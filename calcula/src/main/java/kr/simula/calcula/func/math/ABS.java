907c60c80ef4a47bc530fc49f18e86d613837521
kighie <kighie@gmail.com>
2015-03-25 18:07:34 +0900

    Functions

---
+++ calcula/src/main/java/kr/simula/calcula/func/math/ABS.java
@@ -0,0 +1,34 @@
+/* ******************************************************************************
+ * Copyright (c) 2012 IkChan Kwon kighie@gmail.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package kr.simula.calcula.func.math;
+
+import kr.simula.calcula.func.NumberFunction;
+
+/**
+ * <pre>
+ * </pre>
+ * @author Ikchan Kwon
+ *
+ */
+public class ABS extends NumberFunction{
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public Number eval(Object... args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
