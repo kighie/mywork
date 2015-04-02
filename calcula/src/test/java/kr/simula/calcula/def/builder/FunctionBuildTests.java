3e654b5599e617a129b46328f53392832846fb00
kighie <kighie@gmail.com>
2015-04-02 09:02:55 +0900

    performance

--- calcula/src/test/java/kr/simula/calcula/def/builder/FunctionBuildTests.java
+++ calcula/src/test/java/kr/simula/calcula/def/builder/FunctionBuildTests.java
@@ -18,6 +18,8 @@ import java.math.BigDecimal;
 
 import kr.simula.calcula.ExpressionTests;
 import kr.simula.calcula.core.Gettable;
+import kr.simula.calcula.func.base.MathFunctions;
+import kr.simula.calcula.util.StopWatch;
 
 import org.junit.Test;
 
@@ -29,23 +31,61 @@ import org.junit.Test;
  */
 public class FunctionBuildTests extends ExpressionTests {
 
+	private StopWatch stopwatch = new StopWatch();
+	
 	@Test
 	public void performance(){
+		stopwatch.start();
 		for( int i =0;i<1000;i++){
 			testExpression("=COMBIN( ABS(83/5) ,2)", new BigDecimal("120"));
 		}
+		System.out.println("performance : " + stopwatch.ellapsedTime());
 	}
-
+	
 	@Test
-	public void performance2(){
-		Gettable<?> expression = (Gettable<?>)buildExpression("=COMBIN( ABS(83/5) ,2)");
-		System.out.println();
-		System.out.println(expression.getExpression());
-		System.out.println(expression);
+	public void performanceBuild(){
+		stopwatch.start();
+		for( int i =0;i<1000;i++){
+			buildExpression("=COMBIN( ABS(83/5) ,2)");
+		}
+		System.out.println("performanceBuild : " + stopwatch.ellapsedTime());
+	}
+	
+	Gettable<?> expression = (Gettable<?>)buildExpression("=COMBIN( ABS(83/5) ,2)");
+	@Test
+	public void performanceExec(){
+		stopwatch.start();
+		
+//		System.out.println();
+//		System.out.println(expression.getExpression());
+//		System.out.println(expression);
 		
 		for( int i =0;i<1000;i++){
 			System.out.println(expression.get(null));
 		}
+		System.out.println("performanceExec : " + stopwatch.ellapsedTime());
 	}
-	
+
+	@Test
+	public void javaCode(){
+		stopwatch.start();
+		for( int i =0;i<1000;i++){
+			double value = MathFunctions.combin(MathFunctions.abs(83/5), 2);
+			
+			System.out.println(value);
+		}
+		System.out.println("javaCode : " + stopwatch.ellapsedTime());
+	}
+
+	@Test
+	public void javaCode2(){
+		stopwatch.start();
+		for( int i =0;i<1000;i++){
+			double value = MathFunctions.combin(MathFunctions.abs(83/5), 2);
+			
+			System.out.println(new BigDecimal(value));
+		}
+		System.out.println("javaCode2 : " + stopwatch.ellapsedTime());
+	}
+
 }
